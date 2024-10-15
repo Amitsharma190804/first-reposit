4189ecf1c159a5405508d5f9d852c1403e178cfc
amit <amit.theas@gmail.com>
2024-10-15 15:00:13 +0530

    new line added in python code

---
+++ product.py
@@ -0,0 +1,9 @@
+a1 = float(input("enter the value of a1 : "))
+a2 = float(input("enter the value of a2 : "))
+a3= float(input("enter the value of a3 : "))
+b1 = float(input("enter the value of b1 : "))
+b2 = float(input("enter the value of b2 : "))
+b3 = float(input("enter the value of b3 : "))
+
+dp = (a1*b1 + a2*b2 + a3*b3)
+print ("dot product of two vector is : ",dp)
